133ac365cb7e9ff54604adc1a3ac90a236f7813a
Amirata K <amiratak88@gmail.com>
2018-07-15 04:59:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
 # your code goes here
+def merge_data(keys, data)
+  arr = []
+  keys.each_with_index do |person_hash, i|
+    hash = {}
+    hash[:first_name] = person_hash[:first_name]
+    hash[:awesomeness] = data[0][person_hash[:first_name]][:awesomeness]
+    hash[:height] = data[0][person_hash[:first_name]][:height]
+    hash[:last_name] = data[0][person_hash[:first_name]][:last_name]
+    arr << hash
+  end
+  arr
+end
